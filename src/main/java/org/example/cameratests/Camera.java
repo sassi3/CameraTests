abee0ce9f7a1072b4710f74a543413ff494a7e26
Manuel Sassi <129498641+sassi3@users.noreply.github.com>
2024-05-07 15:34:05 +0200

    Add block separation comments

--- src/main/java/org/example/cameratests/Camera.java
+++ src/main/java/org/example/cameratests/Camera.java
@@ -1,12 +1,14 @@
 package org.example.cameratests;
 
 import org.bytedeco.javacv.CameraDevice;
+import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
 import org.bytedeco.opencv.opencv_core.FileStorage;
 
 public class Camera extends CameraDevice {
     OpenCVFrameGrabber grabber;
 
+    //------ CONSTRUCTOR ------
     public Camera(String name) {
         super(name);
     }
@@ -26,9 +28,17 @@ public class Camera extends CameraDevice {
     public OpenCVFrameGrabber getOpenCVFrameGrabber() {
         return grabber;
     }
-
+    // ----- FRAMEGRABBER METHODS -------
     public void createDefaultOpenCVFrameGrabber() {
         this.grabber = new OpenCVFrameGrabber(0);
+    }
+
+    public OpenCVFrameGrabber getGrabber() {
+        return grabber;
+    }
+
+    public OpenCVFrameGrabber createOpenCvFrameGrabber(int index) throws FrameGrabber.Exception {
+        return new OpenCVFrameGrabber(index);
     }
 
 
